6aa55951cfb32aca74299e9734a53c9cc5264cf2
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-06-06 04:12:41 +0530

    Update index.jsx

--- client/frontend/src/pages/Home/index.jsx
+++ client/frontend/src/pages/Home/index.jsx
@@ -20,7 +20,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_API_URL);
 
 // Home Page Function (All-Merge)
 const Home = () => {
