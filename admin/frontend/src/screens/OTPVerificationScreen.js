8bb3c3f9e5996492b3b425d3ac8857d976bf7a7e
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-06-07 01:20:43 +0530

    Refactor OTPVerificationScreen component

--- admin/frontend/src/screens/OTPVerificationScreen.js
+++ admin/frontend/src/screens/OTPVerificationScreen.js
@@ -26,7 +26,7 @@ const OTPVerificationScreen = () => {
 
     const fetchLogo = async () => {
       try {
-        const API_URL = import.meta.env.VITE_API_URL; 
+        const API_URL = process.env.REACT_APP_API_URL; 
 
         const { data } = await axios.get(`${API_URL}/api/logo`);
         
