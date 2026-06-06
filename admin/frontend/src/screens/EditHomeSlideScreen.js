04ae0f40d09fb192d6f0b590e15d6f7dc5c7b9a0
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-06-07 01:14:17 +0530

    Refactor EditHomeSlideScreen to use env variable

--- admin/frontend/src/screens/EditHomeSlideScreen.js
+++ admin/frontend/src/screens/EditHomeSlideScreen.js
@@ -14,7 +14,7 @@ const EditHomeSlideScreen = () => {
   useEffect(() => {
   const fetchBanner = async () => {
     try {
-      const API_URL = import.meta.env.VITE_API_URL; 
+      const API_URL = process.env.REACT_APP_API_URL; 
 
       const { data } = await axios.get(`${API_URL}/api/banners/${id}`);
       
