35a8881c9cda5ea54df731c65a31f887bbc95db6
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-06-07 01:19:06 +0530

    Refactor ManageLogoScreen to use environment variable

--- admin/frontend/src/screens/ManageLogoScreen.js
+++ admin/frontend/src/screens/ManageLogoScreen.js
@@ -9,7 +9,7 @@ const ManageLogoScreen = () => {
   useEffect(() => {
     const fetchLogo = async () => {
       try {
-        const API_URL = import.meta.env.VITE_API_URL; 
+        const API_URL = process.env.REACT_APP_API_URL; 
 
         const { data } = await axios.get(`${API_URL}/api/logo`);
         
