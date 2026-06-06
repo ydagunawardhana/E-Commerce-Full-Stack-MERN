017b718c7e8d5f0dc073c931eb771b1c074ea1cd
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-06-07 01:06:38 +0530

    Update API_URL to use process.env for React

--- admin/frontend/src/App.js
+++ admin/frontend/src/App.js
@@ -65,7 +65,7 @@ function App() {
 
   const fetchLogo = async () => {
   try {
-    const API_URL = import.meta.env.VITE_API_URL; 
+    const API_URL = process.env.REACT_APP_API_URL; 
 
    
     const { data } = await axios.get(`${API_URL}/api/logo`);
