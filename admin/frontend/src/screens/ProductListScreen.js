9815f93a99bef9d558bf9f7f1508b5d06056f1c2
YDA Gunawardhana <159436360+ydagunawardhana@users.noreply.github.com>
2026-06-07 01:21:10 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- admin/frontend/src/screens/ProductListScreen.js
+++ admin/frontend/src/screens/ProductListScreen.js
@@ -28,7 +28,7 @@ const ProductListScreen = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [selectedProducts, setSelectedProducts] = useState([]);
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = process.env.REACT_APP_API_URL;
 
   // Fetch Data
   const fetchData = async () => {
